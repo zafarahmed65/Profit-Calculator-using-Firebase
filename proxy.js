d6ab2f286bd9d3ed2b7a459ca3ba51da8f6f7c20
zafarahmed65 <zafar.ahmed2k17@gmail.com>
2024-11-18 16:49:30 +0500

    first commit

--- proxy.js
+++ proxy.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+// Serve static files (HTML, CSS, JS) from the root directory
+app.use(express.static(__dirname));
 
+// Serve "index.html" for the root route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Serve other static HTML files like "history.html"
+app.get("/:file", (req, res) => {
+    res.sendFile(path.join(__dirname, req.params.file));
+});
+
+// Proxy setup for Firebase API
 app.use(
     "/firestore",
     createProxyMiddleware({
